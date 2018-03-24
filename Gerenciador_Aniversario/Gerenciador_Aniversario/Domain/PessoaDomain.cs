f8cc5ed66066efdef108c6aade7e853275366fab
Magno Valdetaro <mvaldetaro@gmail.com>
2018-03-24 20:22:54 -0300

    Modelo do Domínio

---
+++ Gerenciador_Aniversario/Gerenciador_Aniversario/Domain/PessoaDomain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gerenciador_Aniversario.Domain
+{
+    public class PessoaDomain
+    {
+        public int PessoaID { get; set; }
+        [Required(ErrorMessage = "O campo nome é obrigatório.")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo Sobrenome é obrigatório.")]
+        public string Sobrenome { get; set; }
+        [Required(ErrorMessage = "O campo Data de Aniversário é obrigatório.")]
+        public DateTime DtAniversario { get; set; }
+    }
+}
