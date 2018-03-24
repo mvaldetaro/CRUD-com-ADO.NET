2a732d9d362d4032f8c50487ea58d1fedaba7222
Magno Valdetaro <mvaldetaro@gmail.com>
2018-03-24 20:23:11 -0300

    Modelo da View

---
+++ Gerenciador_Aniversario/Gerenciador_Aniversario/Models/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gerenciador_Aniversario.Models
+{
+    public class Pessoa
+    {
+        public int PessoaID { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime DtAniversario { get; set; }
+    }
+}
