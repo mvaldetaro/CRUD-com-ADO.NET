f06d9676d2ce87694c41f23230e88b42fff57820
Magno Valdetaro <mvaldetaro@gmail.com>
2018-03-25 09:03:32 -0300

    Calcula dias para o próximo anivesário

---
+++ Gerenciador_Aniversario/Gerenciador_Aniversario/Models/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gerenciador_Aniversario.Models
+{
+    public class Calculator
+    {
+        public static int DiasRestantesAniversario(DateTime dt)
+        {
+            int anoAtual = DateTime.Today.Year;
+
+            var dataInicial = DateTime.Today;
+            var dataFinal = $"{dt.Day}/{dt.Month}/{anoAtual}";
+            var days = (DateTime.Parse(dataFinal).Subtract(dataInicial)).Days;
+
+            if (days <= 0)
+            {
+                days = (DateTime.Parse(dataFinal).AddYears(1).Subtract(dataInicial)).Days;
+            } 
+
+            return days;
+        }
+    }
+}
